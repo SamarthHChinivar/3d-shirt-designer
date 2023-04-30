a7f8cfbbbf919497175fc6b9f43ce6a89df12e98
Samarth H Chinivar <samarthharish321@gmail.com>
2023-05-01 00:16:32 +0530

    backend added

---
+++ server/routes/dalle.routes.js
@@ -0,0 +1,8 @@
+import express from 'express'
+import * as dotenv from 'dotenv'
+import cors from 'cors'
+import { Configuration, OpenAIApi} from 'openai'
+
+dotenv.config();
+
+const router = express.Router();
