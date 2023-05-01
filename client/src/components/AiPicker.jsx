2cd7f5b190869751e6d90964d04d7d934ddead4f
Samarth H Chinivar <samarthharish321@gmail.com>
2023-05-01 23:03:37 +0530

    DALL.E routes and API added

--- client/src/components/AiPicker.jsx
+++ client/src/components/AiPicker.jsx
@@ -1,8 +1,22 @@
 import React from 'react'
+import CustomButton from './CustomButton'
 
-const AiPicker = () => {
+
+const AiPicker = ({prompt, setPrompt, generatingImg, handleSubmit}) => {
   return (
-    <div>AiPicker</div>
+    <div className='aipicker-container'>
+      <textarea
+        placeholder='Ask AI...'
+        className='aipicker-textarea'
+        rows={5}
+        value={prompt}
+        onChange={(e) => setPrompt(e.target.value)}
+      />
+
+      <div className='flex flex-wrap gap-3'>
+
+      </div>
+    </div>
   )
 }
 
