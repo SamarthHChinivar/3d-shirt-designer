293ccef4cd54820d610d89a956c37f924b5b6ce9
Samarth H Chinivar <samarthharish321@gmail.com>
2023-05-20 23:02:17 +0530

    deployment changes

--- client/src/components/AiPicker.jsx
+++ client/src/components/AiPicker.jsx
@@ -1,49 +1,11 @@
-import React from 'react'
-import CustomButton from './CustomButton'
+import React from 'react';
 
-
-const AiPicker = ({prompt, setPrompt, generatingImg, handleSubmit}) => {
+const AiPicker = ({ prompt, setPrompt, generatingImg, handleSubmit }) => {
   return (
-  <div>
-    <a href='./notAvailable.html' target='_blank' rel='noopener noreferrer'>Currently Not Available</a>
-  </div>
-  
-    // <div className='aipicker-container'>
-    //   <textarea
-    //     placeholder='Ask AI...'
-    //     className='aipicker-textarea'
-    //     rows={5}
-    //     value={prompt}
-    //     onChange={(e) => setPrompt(e.target.value)}
-    //   />
+    <div>
+        Currently Not Available
+    </div>
+  );
+};
 
-    //   <div className='flex flex-wrap gap-3'>
-    //     {generatingImg ? (
-    //       <CustomButton
-    //         type='outline'
-    //         title='Asking AI...'
-    //         customStyles='text-xs'
-    //       />
-    //     ) : (
-    //       <>
-    //         <CustomButton
-    //           type='outline'
-    //           title='AI Logo'
-    //           handleClick={() => handleSubmit('logo')}
-    //           customStyles='text-xs'
-    //         />
-
-    //         <CustomButton
-    //           type='filled'
-    //           title='AI Full'
-    //           handleClick={() => handleSubmit('full')}
-    //           customStyles='text-xs'
-    //         />
-    //       </>
-    //     )}
-    //   </div>
-    // </div>
-  )
-}
-
-export default AiPicker
+export default AiPicker;
